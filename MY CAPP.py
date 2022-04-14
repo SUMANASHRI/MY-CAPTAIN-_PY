41d3e467a4a36d18f6dd3f8aac9342b0980ebfb2
SUMANASHRI <101055727+SUMANASHRI@users.noreply.github.com>
2022-04-14 19:42:10 +0530

    Add files via upload

---
+++ MY CAPP.py
@@ -0,0 +1,17 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def area_of_circle(r):
+    pi = 3.14
+    area = pi * r * r
+    return area
+
+print(area_of_circle(5))
+78.5
+fn = input("abc.py")
+      
+abc.py
+f = fn.split(".")
+      
+print("Extension of the file is :" + f[-1])
+      
+Extension of the file is :"python
